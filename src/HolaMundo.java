683ab8984f5766c56e53f8de0a26178df788a34d
CactusTelepatico <adri.rodriguez2499@gmail.com>
2018-11-22 19:45:44 +0100

    Parche sin hola mundo 2

--- src/HolaMundo.java
+++ src/HolaMundo.java
@@ -1,8 +1,7 @@
 
 public class HolaMundo {
 	public static void main(String[] args) {
-		System.out.println("Hola Mundo");
-		System.out.println("Hola Mundo 2");
+		System.out.println("Hola Mundo");		
 	}
 
 }
